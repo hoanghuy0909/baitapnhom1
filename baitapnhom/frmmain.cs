fe835f8636f52ac18543f8ed06519bfd5fd88d8c
Hoang Quang huy <hoangquanghuya6@gmail.com>
2020-04-12 16:21:23 +0700

    Initial commit

---
+++ baitapnhom/frmmain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace baitapnhom
+{
+    public partial class frmmain : Form
+    {
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        private void chiTiếtThuêSáchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmchitietthuesach frm = new frmchitietthuesach();
+            frm.Show();
+        }
+
+        private void sáchTruyệnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmsachtruyen1 frm = new frmsachtruyen1();
+            frm.Show();
+        }
+    }
+}
